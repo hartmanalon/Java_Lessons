5d05281e73244dce4d52ab0ab20b100a4cfecb7f
alonh <alon.hartman@cellebrite.com>
2018-04-30 16:47:24 +0300

    OkHttpDemo

---
+++ OkHttpClient3/src/main/java/OkHttpDemo.java
@@ -0,0 +1,30 @@
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public class OkHttpDemo {
+
+    OkHttpClient client;
+
+    private String run(String url) throws IOException {
+        if(client == null) {
+            client = new OkHttpClient();
+        }
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        OkHttpDemo example = new OkHttpDemo();
+        String response = example.run("https://raw.github.com/square/okhttp/master/README.md");
+        System.out.println(response);
+    }
+}
